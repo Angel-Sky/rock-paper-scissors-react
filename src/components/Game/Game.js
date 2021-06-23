514d3896ddda60f64f12052a63a9d0f7c9823feb
Iva Tosheva <otevi55@gmail.com>
2021-06-23 17:09:10 +0300

    implemented user choice

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,34 @@
-function Start() {
+import { useEffect, useState } from 'react';
+import { Col, Row, Button } from 'react-bootstrap';
+
+function Game() {
+    const [score, setScore] = useState(0);
+    const [selected, setSelected] = useState('');
+
+    const handleSelect = (e) => {
+        e.preventDefault();
+        setSelected(e.target.innerText)
+    }
+
+    useEffect(() => {
+        console.log(selected)
+    }, [selected])
+
     return (
-        <div className="container" id="categories">
-           <h1>Game</h1>
+        <div className="container">
+            <h1>Game</h1>
+            <Row>
+                <Col lg={6} sm={12} >
+                    <Button variant="dark" onClick={handleSelect}>Rock</Button>
+                    <Button variant="dark" onClick={handleSelect}>Paper</Button>{' '}
+                    <Button variant="dark" onClick={handleSelect}>Scissors</Button>{' '}
+                </Col>
+                <Col lg={6} sm={12} >
+                
+                </Col>
+            </Row>
         </div>
     )
 }
 
-export default Start;
+export default Game;
