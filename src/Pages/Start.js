2b59188ccdaebae26d02c4fb49bac7052350f1c3
Iva Tosheva <otevi55@gmail.com>
2021-06-24 22:51:54 +0300

    Added CSS to Start page

--- src/Pages/Start.js
+++ src/Pages/Start.js
@@ -1,12 +1,19 @@
 import { Link } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
-
+import { Row, Col, Button } from 'react-bootstrap';
+import '../components/Start/Start.css'
 function Start() {
     return (
-        <div className="container" id="categories">
-            <h1>Start</h1>
+        <div className="container text-center">
+            <h1 id="main-heading">Lets Play Rock Paper Scissors!</h1>
+            <Row>
+                <Col lg={12} sm={12} className="weapons">
+                    <img src="/rock.png" alt="rock" />
+                    <img src="/paper.png" alt="paper" />
+                    <img src="/scissors.png" alt="scissors" />
+                </Col>
+            </Row>
             <Link to="/game">
-                <Button variant="dark">Start</Button>{' '}
+                <Button variant="dark" id="btn-start">Start</Button>{' '}
             </Link>
         </div>
     )
