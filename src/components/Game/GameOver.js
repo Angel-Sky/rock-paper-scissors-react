5a051bb3bcedb0260b1c293b73d39b5f6ee0efd6
Iva Tosheva <otevi55@gmail.com>
2021-06-25 14:19:33 +0300

    Added images and CSS for GameOver component

--- src/components/Game/GameOver.js
+++ src/components/Game/GameOver.js
@@ -1,17 +1,24 @@
-import { Button } from 'react-bootstrap';
+import { Button, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './GameOver.css';
 
 function GameOver({ userScore }) {
     return (
-        <>
+        <Col lg={12} sm={12} className="text-center">
             {userScore >= 3 ?
-                <p>You Win!</p> :
-                <p>Computer Wins!</p>
+                <div>
+                    <img src="/user-wins.png" alt="Happy sun for winning the game" />
+                    <h2 className="winner-msg">You Win!</h2>
+                </div> :
+                <div>
+                    <img src="/comp-wins.png" alt="Happy computer for winning the game" />
+                    <h2 className="winner-msg">The Computer Wins!</h2>
+                </div>
             }
             <Link to="/">
                 <Button variant="dark">Start Over</Button>
             </Link>
-        </>
+        </Col>
     )
 }
 
